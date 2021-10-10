3cc5bfaef184b6b323b3f9ff3ed828ccb1302630
Trite <pbrundog@gmail.com>
2021-10-10 08:16:06 -0700

    .

--- 2015/day-02-p1.hs
+++ 2015/day-02-p1.hs
@@ -1,25 +1,57 @@
-import Data.List.Split
+splitOn' :: [String] -- ^ accList - previous accumulations
+  -> [Char] -- ^ acc - current accumulator
+  -> Char -- ^ split - character to split on
+  -> [Char] -- ^ rest
+  -> [String]
+splitOn' accList acc _ [] = reverse $ reverse acc:accList
+splitOn' accList acc split (x:rest)
+    | split == x = splitOn' (reverse acc:accList) [] split rest
+    | otherwise  = splitOn' accList (x:acc) split rest
 
-testing12 = splitOn 'x' "4x3x2"
+splitOn :: Char -> [Char] -> [String]
+splitOn = splitOn' [] []
 
--- >>> testing12
+-- >>> splitOn 'x' "123x2x234"
+-- ["123","2","234"]
 
-area w l h = 2*w*l + 2*w*h + 2*l*h + slack w l h
-    where
-        slack w l h = minimum [w*l, w*h, l*h]
+intify :: String -> Int
+intify x = read x :: Int
 
--- >>> area 2 3 4
--- >>> area 1 1 10
+-- >>> intify "23"
+-- 23
+-- >>> map intify $ splitOn 'x' "123x2x234"
+-- [123,2,234]
+
+area :: (Num p, Ord p) => [p] -> p
+area [w, l, h] = 2*w*l + 2*w*h + 2*l*h + slack w l h
+    where slack w l h = minimum [w*l, w*h, l*h]
+area other = error "You're doing it wrong!"
+
+-- >>> area $ map intify $ splitOn 'x' "2x3x4"
 -- 58
+
+calc :: [Char] -> Int
+calc str = area $ map intify $ splitOn 'x' str
+
+-- >>> calc "2x3x4"
+-- 58
+-- >>> calc "1x1x10"
 -- 43
-
-
-
--- tabulate toCheck = map area toCheck
-
--- >>> tabulate [()]
 
 main = do
     contents <- readFile "day-02-input.txt"
-    let result = length . lines $ contents
+    let result = sum $ map calc $ lines contents
     return result
+
+{-
+PS C:\Git\advent-of-code\2015> ghci .\day-02-p1.hs
+GHCi, version 8.10.7: https://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( day-02-p1.hs, interpreted )
+Ok, one module loaded.
+*Main> main
+1606483
+-}
+
+
+
+
