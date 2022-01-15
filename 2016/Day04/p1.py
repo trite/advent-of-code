8b3d5f73088ca28838681923ef193da0bdea8137
trite <trite@users.noreply.github.com>
2022-01-15 15:21:45 -0700

    Forgot to commit this the other day

--- 2016/Day04/p1.py
+++ 2016/Day04/p1.py
@@ -14,13 +14,49 @@ class Room:
     value: str
     sectorId: int
     checksum: str
+    actualChecksum: str
+
+valueStr = 'value'
+sectorStr = 'sectorId'
+checksumStr = 'checksum'
+
+def check_sum(roomValue: str) -> str:
+    counts = count_chars(roomValue)
+    sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+    return ''.join([x[0] for x in sorted_counts[:5]])
 
 def parse_line(line: str):
-    pattern = re.compile(r'(?P<value>[a-z\-]+)-(?P<sectorId>\d+)\[(?P<checksum>[a-z]+)\]')
+    pattern = re.compile(rf'(?P<{valueStr}>[a-z\-]+)-(?P<{sectorStr}>\d+)\[(?P<{checksumStr}>[a-z]+)\]')
     result = pattern.match(line)
+
     if result:
-        return Room(result.group('value'), int(result.group('sectorId')), result.group('checksum'))
+        roomValue = result.group(valueStr).replace('-', '')
+
+        return Room(roomValue,
+            int(result.group(sectorStr)),
+            result.group(checksumStr),
+            check_sum(roomValue))
     else:
         raise Exception(f'Could not parse line: {line}')
 
-print(parse_line("aaaaa-bbb-z-y-x-123[abxyz]"))
+
+testList = """aaaaa-bbb-z-y-x-123[abxyz]
+a-b-c-d-e-f-g-h-987[abcde]
+not-a-real-room-404[oarel]
+totally-real-room-200[decoy]"""
+
+def run (rawList: str) -> int:
+    lines = rawList.split('\n')
+
+    return sum([
+        x.sectorId
+        for line in lines
+        if (x := parse_line(line)).actualChecksum == x.checksum])
+
+print(f'test data: {run(testList)}')
+
+f = open('2016\\Day04\\input.txt', 'r')
+lines = f.read()
+result = run(lines)
+
+print(f'actual result: {result}')
