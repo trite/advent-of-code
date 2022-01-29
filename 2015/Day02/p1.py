b72afe27c976233503e4fb0cbe2015992e2f304a
trite <trite@users.noreply.github.com>
2022-01-28 17:47:37 -0700

    Cleanup

--- 2015/Day02/p1.py
+++ 2015/Day02/p1.py
@@ -17,25 +17,15 @@ def calc_paper(box: str) -> int:
 def solve(func: Callable[[str], int], boxes: str) -> int:
     return sum(list(map(func, boxes.split('\n'))))
 
-# def run_p1(boxes: str) -> int:
-#     return sum(list(map(calc_paper, boxes.split('\n'))))
-
-# run_p1 = partial(solve, calc_paper)
-
 with open('2015\\Day02\\input.txt', 'r') as f:
     content = f.read()
 
 print(f'P1 answer: {solve(calc_paper, content)}')
-# print(f'P1 answer: {run_p1(content)}')
 # P1 answer: 1606483
 
 def calc_ribbon(box: str) -> int:
     a,b,c = parse(box)
     return 2*a + 2*b + a*b*c
 
-# def run_p2(boxes: str) -> int:
-#     return sum(list(map(calc_ribbon, boxes.split('\n'))))
-
-# run_p2 = partial(solve, calc_ribbon)
-
 print(f'P2 answer: {solve(calc_ribbon, content)}')
+# P2 answer: 3842356
