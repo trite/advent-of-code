e5549919a3652f684343ab280796ab315888b945
trite <trite@users.noreply.github.com>
2022-02-01 19:50:07 -0700

    Messing around with pillow some more

--- 2015/Day06/p1.py
+++ 2015/Day06/p1.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
-from enum import Enum
-from typing import Tuple
+from typing import Literal, Tuple
 from PIL import Image
 import numpy
 
@@ -8,19 +7,15 @@ test = Image.new(mode="L", size=(100,100))
 # test.show()
 # test2 = Image.fromarray(numpy.array("i", [x for x in range(10000)]), "L") # type: ignore
 
-size = 1000
-numpy.random.seed(1)
-arr = numpy.random.rand(size, size)
+# size = 1000
+# numpy.random.seed(1)
+# arr = numpy.random.rand(size, size)
+arr = numpy.fromfunction(lambda x, y: y / 1000, (1000, 1000), dtype = float) # type: ignore
 Image.fromarray(arr, mode='L').show() # type: ignore
 
 # test2.show()
 
 Position = Tuple[int,int]
-
-class CommandType(Enum):
-    Off = 0
-    On = 1
-    Toggle = 2
 
 def parse_position(rawPair: str) -> Position:
     match rawPair.split(','):
@@ -28,6 +23,8 @@ def parse_position(rawPair: str) -> Position:
             return int(x),int(y)
         case other:
             raise Exception(f'Check your assumptions, this should always be a Tuple! {other}')
+
+CommandType = Literal["On", "Off", "Toggle"]
 
 @dataclass
 class Command:
@@ -49,13 +46,13 @@ testCmds = [
 def parse_line(line: str) -> Command:
     match line.split(' '):
         case ['turn', 'on', start, 'through', end]:
-            return Command(CommandType.On, start, end)
+            return Command("On", start, end)
 
         case ['turn', 'off', start, 'through', end]:
-            return Command(CommandType.Off, start, end)
+            return Command("Off", start, end)
 
         case ['toggle', start, 'through', end]:
-            return Command(CommandType.Toggle, start, end)
+            return Command("Toggle", start, end)
 
         case other:
             raise Exception(f'Unable to parse: {other}')
